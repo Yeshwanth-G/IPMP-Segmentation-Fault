8f5e792e9defef7d60f088c919d683010256ad48
Yeshwanth <87797634+Yeshwanth-G@users.noreply.github.com>
2021-11-03 17:10:18 +0530

    Upadate with day-1,task-1
    Added middle element,reverse print and reverse linked list problems

--- Week1/01-11-21/linedlist.cpp
+++ Week1/01-11-21/linedlist.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
-#include "linkedlist.h"//including custom headerfile
+#include "linkedlist.h"
 using namespace std;
-using namespace llist;//custom namespace
+using namespace llist;
 int main(){
 linkedlist l;
 vector<int>v={1,2,3,4,5};
@@ -13,8 +13,15 @@ l.printlist();
 l.deletenode(2);//delete node at postion 2(given position)
 l.printlist();
 cout<<"Length="<<l.length()<<endl;
-if(l.search(3))cout<<"Found 3!!";
-else cout<<"Not Found 3!!";
+int a=10;
+if(l.search(a))cout<<"Found "<<a<<endl;
+else cout<<"Not Found "<<a<<endl;
+cout<<"Middle element:"<<l.MiddleElement()<<endl;
+cout<<"Reverse print without actual reversing:";
+l.PrintRev();
+l.reverse();
+cout<<"reversed list:";
+l.printlist();
 l.deletelist();//deletes entire list and makes head as NULL;
 return 0;
 }
