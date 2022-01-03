850b54fb1551cbd3de9c27d08188affb9fee366c
Yeshwanth <87797634+Yeshwanth-G@users.noreply.github.com>
2022-01-03 15:47:01 +0530

    Update Missing number.cpp

--- Week-4/Day2/Missing number.cpp
+++ Week-4/Day2/Missing number.cpp
@@ -1,9 +1,9 @@
 int MissingNumber(vector<int>& array, int n) {
-        // Approach 1:find sum of all array elements and subract it from n*(n+1)/2
+        // Approach 1:find sum of all array elements and subract it from n*(n+1)/2===>Time:O(n),space-O(1)
         // int x=accumulate(array.begin(),array.end(),0);
         // int sum=(n*(n+1))/2;
         // return sum-x;
-        //Approach 2:find xor of all array elements and extract missing value;
+        //Approach 2:find xor of all array elements and extract missing value;==>O(n),O(1);
         // int x=0,y=0;
         // for(int i=0;i<n-1;i++){
         //     x=x^array[i];
