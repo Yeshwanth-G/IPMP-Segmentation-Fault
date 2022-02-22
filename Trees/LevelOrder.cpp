6887fdd542336ba3cf43d5dd5e6b7bff7d5e0e57
Yeshwanth <87797634+Yeshwanth-G@users.noreply.github.com>
2022-02-22 22:24:51 +0530

    Create LevelOrder.cpp

---
+++ Trees/LevelOrder.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>vec;
+        vector<int>t;
+        deque<TreeNode*>d;
+        if(root==NULL)return vec;
+        d.push_back(root);
+        d.push_back(NULL);
+        int i=0;
+         TreeNode*p=root;
+        while(!d.empty()){
+            if(i%2==0){
+                p=d.front();
+                if(p==NULL){  
+                    i++;
+                    vec.push_back(t);
+                    t.clear();
+                    if(d.size()<2)break;
+                    continue;
+                }
+                
+                if(p->left)d.push_back(p->left); 
+                if(p->right)d.push_back(p->right);
+                d.pop_front();
+                t.push_back(p->val);
+            }
+            else {
+                 p=d.back();
+                if(p==NULL){
+                    i++;
+                    vec.push_back(t);
+                    t.clear();
+                    if(d.size()<2)break;
+                    continue;
+                }
+                
+                if(p->right)d.push_front(p->right);
+                if(p->left)d.push_front(p->left);
+                d.pop_back();
+                t.push_back(p->val);
+            }
+            }
+        return vec;
+    }
+};//O(n),O(n)
