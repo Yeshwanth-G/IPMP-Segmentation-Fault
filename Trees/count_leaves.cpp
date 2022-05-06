4dc8fca9636540b54c169ba67106fd80cd282fcf
Yeshwanth <87797634+Yeshwanth-G@users.noreply.github.com>
2022-05-06 17:18:06 +0530

    Create count_leaves.cpp

---
+++ Trees/count_leaves.cpp
@@ -0,0 +1,7 @@
+
+int countLeaves(Node* root)
+{
+    if(root==nullptr)return 0;
+    if(root->left==nullptr&&root->right==nullptr)return 1;
+    return countLeaves(root->left)+countLeaves(root->right);
+}//O(N),O(N)
