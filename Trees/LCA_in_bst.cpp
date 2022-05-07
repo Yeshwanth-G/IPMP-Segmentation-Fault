e979f563586a42dbf19bce5fec2abe70db3aa255
Yeshwanth <87797634+Yeshwanth-G@users.noreply.github.com>
2022-05-07 20:35:55 +0530

    Update LCA_in_bst.cpp

--- Trees/LCA_in_bst.cpp
+++ Trees/LCA_in_bst.cpp
@@ -6,3 +6,13 @@ Node* LCA(Node *root, int n1, int n2)
   if(root->data<min(n1,n2))return LCA(root->right,n1,n2);
   if(root->data>max(n1,n2))return LCA(root->left,n1,n2);
 }
+--------------------------------------------------------------
+Iterative:
+Node* LCA(Node *root, int n1, int n2)
+{
+  while(root!=nullptr){
+  if(root->data>=min(n1,n2)&&root->data<=max(n1,n2))return root;
+  if(root->data<min(n1,n2))root=root->right;
+  if(root->data>max(n1,n2))root=root->left;}
+  return root;
+}
