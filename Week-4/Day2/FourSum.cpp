6568c1f6b6bcd41b30f3dfaa926323122f2cd5c4
Yeshwanth <87797634+Yeshwanth-G@users.noreply.github.com>
2022-06-02 15:05:09 +0530

    Update FourSum.cpp

--- Week-4/Day2/FourSum.cpp
+++ Week-4/Day2/FourSum.cpp
@@ -1,79 +1,91 @@
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
-#include<vector>
-using namespace std;
-vector<int> twosum(int a[], int p,int q, int n, int x)
+//Approach 1:take all quadraples and find sum-O(n^4),O(1)
+bool find4Numbers(int v[], int n, int X)  
 {
-    // Approch 1:two pointer approach
-    // int i=p+1,j=n-1;
-    // while(i<j){
-    //     if(a[j]+a[i]<x)
-    //     i++;
-    //     else if(a[i]+a[j]>x)
-    //     j--;
-    //     else {
-    //         if(p!=i&&p!=j)
-    //         return 1;
-    //     }
-    // }
-    // return 0;
-    //Approach 2:using extra space
-    unordered_map<int, int> hmap;
-    vector<int>v;
-    for (int i = 0; i < n; i++)
-    {
-        if (i != p&&i!=q)
-        {
-            if (hmap.empty() || hmap.find(x - a[i]) == hmap.end()){
-                hmap[a[i]];
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                for(int b=k+1;b<n;b++){
+                    if(v[i]+v[j]+v[k]+v[b]==X)return true;
                 }
-            else{
-            v.push_back(a[i]);
-            v.push_back(x-a[i]);
-                return v;
-                }
+            }
         }
     }
-    return v;
+    return false;
 }
-void triplet(int a[], int n, int X)
+------------------------------------------------------------------
+//Approach 2:fix two numbers and apply two for other two-O(n^3),O(1)
+bool find4Numbers(int v[], int n, int X)  
 {
-    //Approach1:for every pair a[i],a[j]...find if other two numbers exists such that sum gives x==>time:O(n^3),space:O(1)/O(n)
-    // // sort(A,A+n);
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for(int j=i+1;j<n;j++){       
-    //     if (!twosum(A, i,j, n, X-A[i]-A[j]).empty()){
-    //         vector<int>v=twosum(A, i,j, n, X-A[i]-A[j]);
-    //             cout<<v[0]<<" "<<v[1]<<" "<<A[i]<<" "<<A[j];
-    //         return ;}
-    //     }
-    // }
-    // cout<<"-1";
-    // return ;
-    //Approach:2=>for every pair a[i],a[j] find sum and then iterate in that sum array and find two elements which gives sum as x;
+    sort(v,v+n);
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int target=X-v[i]-v[j];
+            int s=j+1;
+            int e=n-1;
+            while(s<e){
+                if(v[s]+v[e]==target)return true;
+                if(v[s]+v[e]<target)s++;
+                else e--;
+            }
+        }
+    }
+    return false;
+}
+---------------------------------------------------------------------
+ //Approach 3:store all possible pairs sum in a array and then apply two sum on that array.
+ //since array stores all possible pair sum its size is O(N^2)
+ //so sorting it will take O(n^2logn) time:time=O(n^2logn),O(N^2)space
+ //suppose we got an anser as {arr[1]+arr[2]},{arr[2]+arr[3]} here these cannot form 4 distinct numbers-handle this explicity.
+bool diff(pair<int,pair<int,int>>p1,pair<int,pair<int,int>>p2){
+    if(p1.second.first==p2.second.first||p1.second.first==p2.second.second)return false;
+    if(p1.second.second==p2.second.first||p1.second.second==p2.second.second)return false;
+    return true;
+}
+bool find4Numbers(int v[], int n, int X)  
+{
+    
+    vector<pair<int,pair<int,int>>>temp;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            temp.push_back(make_pair(v[i]+v[j],make_pair(i,j)));
+        }
+    }
+    sort(temp.begin(),temp.end());
+    int s=0,e=temp.size()-1;
+    while(s<e){
+        int x=temp[s].first+temp[e].first;
+        if(X==x&&diff(temp[s],temp[e]))return true;
+        if(x<X)s++;
+        else e--;
+    }
+    
+    return false;
+}
+---------------------------------------------------------------------------------
+ //instead of storing all pairs sum in array and sorting,sotre in a hashmap
+ //now for every pair find if X-pairsum exists in hashmap.
+ //O(N^2),O(N^2)
+bool diff(pair<int,int>p1,pair<int,int>p2){
+    if(p1.first==p2.first||p1.first==p2.second)return false;
+    if(p1.second==p2.first||p1.second==p2.second)return false;
+    return true;
+}
+bool find4Numbers(int v[], int n, int X)  
+{
     unordered_map<int,pair<int,int>>hmap;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            if(hmap.empty()||hmap.find(X-a[i]-a[j])==hmap.end()){
-                pair<int,int> p=make_pair(i,j);
-                hmap[a[i]+a[j]]=p;
-            }
-            else{
-                pair <int,int>p=hmap.find(X-a[i]-a[j])->second;
-                if(i!=p.first&&i!=p.second&&j!=p.first&&j!=p.second){
-                    cout<<a[i]<<" "<<a[j]<<" "<<a[p.first]<<" "<<a[p.second]<<endl;
-                    return ;
-                }
-            }
+            int vl=v[i]+v[j];
+            hmap[vl]={i,j};
+            
         }
     }
-    return;
-}
-int main()
-{
-int arr[5]={1,2,3,4,5};
-triplet(arr,5,10);
-    return 0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int vl=v[i]+v[j];
+            if(hmap.find(X-vl)!=hmap.end())
+            if(diff(make_pair(i,j),hmap[X-vl]))return true;
+        }
+    }
+    return false;
 }
