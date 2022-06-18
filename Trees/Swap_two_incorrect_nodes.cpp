4e18b3c65567ecd1e3f12da9366849ae0f102c87
Yeshwanth <87797634+Yeshwanth-G@users.noreply.github.com>
2022-06-18 18:47:38 +0530

    Update Swap_two_incorrect_nodes.cpp

--- Trees/Swap_two_incorrect_nodes.cpp
+++ Trees/Swap_two_incorrect_nodes.cpp
@@ -19,3 +19,25 @@ void inorder(Node*root,Node*&prev,Node*&p1,Node*&p2,Node*&temp){
         
         swap(r1->data,r2->data);
     }
+-------------------------------------------------------------------------------------------------------------------------------------
+//without temp.
+void inorder(Node*root,Node*&prev,Node*&p1,Node*&p2){
+      if(root==nullptr)return;
+      inorder(root->left,prev,p1,p2);
+      if(prev==nullptr)prev=root;
+      if(prev->data>root->data){
+          if(p1==nullptr){p1=prev;p2=root;}
+          else p2=root;
+      }
+      prev=root;
+      inorder(root->right,prev,p1,p2);
+  }
+    void correctBST( struct Node* root )
+    {
+        Node*r1=nullptr,*r2=nullptr,*p=nullptr,*temp=nullptr;
+        inorder(root,p,r1,r2);
+        if(r2==nullptr){
+            r2=temp;
+        }
+        swap(r1->data,r2->data);
+    }
